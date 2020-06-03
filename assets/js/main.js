ca0ee159fc8b31fe72d54021e511710e8bf12f5b
Deepak B S <66330710+stensoftacademy@users.noreply.github.com>
2020-06-03 19:07:28 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,32 @@
-/*
+
+const publicKey = '<public key from Push Companion>';/*
 	Broadcast by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
+});
+navigator.serviceWorker && navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {  
+  
+serviceWorkerRegistration.pushManager.getSubscription()  
+    .then(function(subscription) {  
+     if (subscription) {
+        console.info('Got existing', subscription);
+        window.subscription = subscription;
+        return;  // got one, yay
+      }
+
+      const applicationServerKey = urlB64ToUint8Array(publicKey);
+      serviceWorkerRegistration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey,
+      })
+        .then(function(subscription) { 
+          console.info('Newly subscribed to push!', subscription);
+          window.subscription = subscription;
+        });
+    });
 });
 
 (function($) {
