67e8c692000d43227038d865276df301a5f32e24
Deepak B S <66330710+stensoftacademy@users.noreply.github.com>
2020-06-03 19:16:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,3 +8,17 @@ self.addEventListener('push', function(event) {
       body: 'Push Message received'
    }));
 });
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('the-magic-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/STENSOFT.png',
+        '/assets/js/main.js',
+        '/assets/css/main.css',
+      ]);
+    })
+  );
+});
